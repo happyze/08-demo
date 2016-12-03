4b32bbf999a7bf62dd592c01cddb2e343e9b79cc
happyze <519939143@qq.com>
2016-12-03 11:08:00 +0800

    -aa

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import BlogCard from '../component/BlogCard';
+let data=[
+  {index:1,title:"hello",desc:"11111"},
+  {index:1,title:"hello",desc:"2222222"},
+  {index:1,title:"hello",desc:"33333333"}
+]
 class Blog extends React.Component {
   render () {
+    let cards=data.map((item,i) => <BlogCard {...item} key={i} />)
     return(
-      <div>
-          Blog
+      <div className="blog-warp">
+        {cards}
       </div>
     )
   }
