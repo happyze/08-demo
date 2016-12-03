900e5e5004011c66ee7d75f7a6a626fea5f1a9e5
happyze <519939143@qq.com>
2016-12-03 11:47:17 +0800

    -aaa

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+import Loading from '../component/Loading';
 import BlogCard from '../component/BlogCard';
-let data=[
-  {index:1,title:"hello",desc:"11111"},
-  {index:1,title:"hello",desc:"2222222"},
-  {index:1,title:"hello",desc:"33333333"}
-]
 class Blog extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      data:[],
+      wait:true
+    }
+  }
+  componentDidMount(){
+    axios.get("https://raw.githubusercontent.com/happyze/08-demo/master/data/blogcard.json")
+      .then(res=> this.setState({data:res.data,wait:false}))
+  }
   render () {
-    let cards=data.map((item,i) => <BlogCard {...item} key={i} />)
+    // let cards=data.map((item,i) => <BlogCard {...item} key={i} />)
     return(
       <div className="blog-warp">
-        {cards}
+        {this.state.wait?<Loading />:this.state.data.map((item,i) =><BlogCard {...item} key={i}/>)}
       </div>
     )
   }
--- src/component/Loading.js
+++ src/component/Loading.js
@@ -3,7 +3,7 @@ import CircularProgress from 'material-ui/CircularProgress';
 class Loading extends React.Component {
   render () {
     return(
-      <div>
+      <div style={{textAlign:"center"}}>
         <CircularProgress size={40} thickness={4} color="#AFB42B"/>
       </div>
     )
