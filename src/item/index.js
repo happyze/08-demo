a351dd0eb633f4f8240e8c6c3e16c086c76b875c
happyze <519939143@qq.com>
2016-12-04 09:36:59 +0800

    ada

--- src/item/index.js
+++ src/item/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import marked from 'marked';
+import axios from 'axios';
+import Loading from '../component/Loading';
 class Item extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      data:""
+    }
+  }
+  componentDidMount(){
+    let address = this.props.params.title;
+    axios.get(`https://raw.githubusercontent.com/happyze/08-demo/master/data/${address}.md`)
+    .then(res => this.setState({data:res.data}))    
+  }
   render () {
-    let content = this.props.params.title==0?"这是第一个页面":
-                  this.props.params.title==1?"这是第二哥页面":
-                  this.props.params.title==2?"这是第三哥页面":"第n个页面"
     return(
       <div>
-        {content}
-        {marked("# 666666")}
-        <div dangerouslySetInnerHTML={{__html:marked("# 666666")}} />
+        {
+          this.state.data.length == 0?<Loading/>:
+          <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}/>
+        }
       </div>
     )
   }
